0859d7c46b572d95b0933fb95312f44310e0aff3
Drifter0071 <145994312+Drifter0071@users.noreply.github.com>
2024-10-26 15:26:10 +0200

    hoping it's fixed lol

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -8,16 +8,17 @@ function Login() {
     <body>
       <div id="fieldset-wrapper">
         <h1 id="title">Login</h1>
+        <p>Don't have an account yet? <Link id="link" to="/register">Register!</Link></p> <br />
         <div id="login-fieldset">
             <form action="/api/uauth:def">
-              <p>Don't have an account yet? <Link id="link" to="/register">Register!</Link></p> <br />
               <label htmlFor="email">e-mail</label> <br />
               <input type="email" id="email" name="email" required placeholder=':/' /> <br />
               <label htmlFor="password">password</label>   <br />
               <input type="password" id="password" name="password" required placeholder=':/' /> <br />
               <p  id='error-field'></p> <br />
-              <button type="submit" id="login-button">LOGIN➢</button> <button id="discord-auth-button">Connect DISCORD</button>
+              <button type="submit" id="login-button">LOGIN➢</button> 
             </form>
+            <button id="discord-auth-button"><img src='../assets/discord.png' alt="Discord Icon"  width="200" height="50"/></button>
           </div>
         </div>
     </body>
